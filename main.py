f5ca44eeca81d666fec6d603fd0445a451adef7f
Jason Poirier Lavoie <jpoirierlavoie@gmail.com>
2021-03-02 10:21:52 -0500

    Post templates

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, make_response
+from flask import Flask, render_template, url_for, make_response, redirect
 from google.cloud import datastore
 
 app = Flask(__name__)
